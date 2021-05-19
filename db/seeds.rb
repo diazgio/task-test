1bca55e63397246af857ab8634e6d4d63c08b2e3
Giordano Dìaz <diazgiordano@gmail.com>
2021-05-19 18:30:33 -0500

    Add seeds to data base

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Commit.create(user_name: 'diazgio', repo_name: 'task-test')
+Commit.create(user_name: 'diazgio', repo_name: 'task-test-front')
